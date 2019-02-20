3511355c45a92569fbca86392dc6fb53f95bd2c8
tgray017 <tgray017@gmail.com>
2019-02-20 13:35:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,10 @@
 class Application
   def call(env)
-    if Time.now.hour 
+    resp = Rack::Response.new
+    if Time.now.hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
   end
 end
